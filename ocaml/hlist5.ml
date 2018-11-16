1c4b60ea2bc350043e21010ad7029d2aacbd1339
Oliver Bracevac <bracevac.git@gmail.com>
2018-11-16 10:42:51 +0100

    Working variadic core join shape

--- ocaml/hlist5.ml
+++ ocaml/hlist5.ml
@@ -1,7 +1,9 @@
 #use "topfind";;
 #mod_use "utility.ml";;
-#mod_use "handlers.ml";;
 #mod_use "count.ml";;
+#mod_use "delimcont.ml";;
+#mod_use "async.ml";;
+
 
 (* Lightweight HLists *)
 
@@ -127,10 +129,34 @@ let _ =
  (3, ("B", (0.3, ()))); (3, ("B", (0.2, ()))); (3, ("B", (0.1, ())))]
 *)
 
+(* ===================== Oliver's stuff ===================== *)
 
-(* emulate the type constructors in our framework *)
-type 'a r = 'a list
+(* Utility functions for generating and composing effect handlers. *)
+
+(* TODO backport to handlers.ml *)
+module Handlers = struct
+  (* binary handler composition *)
+  let (|+|) h1 h2 action = h1 (fun () -> h2 action)
+  (* compose given list of handlers to a single handler.  *)
+  let comp hs = List.fold_right (|+|) hs (fun action -> action ())
+  (* single handler application *)
+  let with_h h action = h action
+  (* multiple handler application  *)
+  let with_hs hs action = with_h (comp hs) action
+  (* apply handler-generating function to a list of things and compose *)
+  let gen list f = comp (List.mapi f list)
+end
+
+(* Emulates type constructors in our framework *)
+type 'a react = 'a list
 type 'a evt = 'a
+module Reacts = struct
+  include HList(struct type 'a t = 'a react end)
+  let rec eat f = function (* TODO needs to change to proper stream type *)
+    | [] -> ()
+    | x :: xs -> (f x); eat f xs
+end
+module Events = HList(struct type 'a t = 'a evt end)
 
 (* a slot exposing a generative effect, just as in our framework *)
 module type SLOT = sig
@@ -144,6 +170,7 @@ module type SLOT = sig
 end
 type 'a slot = (module SLOT with type t = 'a)
 type slot_ex = (module SLOT)
+(* Create a slot instance from a value witnessing the type. *)
 let mkSlot (type s) (t: s) =
   (module struct
      type t = s
@@ -165,11 +192,13 @@ end
 
 module Slots = struct
   include HList(struct type 'a t = 'a slot end)
+  (* Forget the concrete types of the slot hlist for uniform processing. *)
   let rec abstract: type a. a hlist -> slot_ex list =
     function
     | Z -> []
-    | S (slot,hs) -> (Obj.magic slot) :: (abstract hs)
+    | S (slot,hs) -> (Obj.magic slot) :: (abstract hs) (* It's weird that a cast is required in this scenario. *)
 
+  (* Wrap effect thunks in list drefs. *)
   let rec mailboxes: type a. a hlist -> a ListDrefs.hlist =
     function
     | Z -> ListDrefs.nil
@@ -178,34 +207,27 @@ module Slots = struct
        ListDrefs.cons (S.getMail) (mailboxes hs)
 end
 
-module Tuple = HList(struct type 'a t = 'a evt end)
-
 (* We assume this will be generated from user query. *)
 module type JOIN = sig
   type index
   val slots: index Slots.hlist
-  val show: index -> string
 end
 
-(* module Handlers = struct
- *   (\* compose given list of handlers to a single handler. *\)
- *   let hcomp hs =
- *     let comp h hres = (fun action -> h (fun () -> hres action)) in
- *     List.fold_right comp hs (fun action -> action ())
- *
- *   (\* apply handler-generating function to a list of things and compose *\)
- *   let gen list f = hcomp (List.mapi f list)
- * end *)
-
-module JoinImpl(J: JOIN) = struct
+(* Arity generic join implementation. *)
+module JoinShape(J: JOIN) = struct
   include J
   let mboxes = Slots.mailboxes slots
   let slot_list = Slots.abstract slots
 
-  effect Trigger: index Tuple.hlist -> unit
-  let trigger v = perform (Trigger v)
+  effect Trigger: index -> unit
+  let trigger tuple = perform (Trigger tuple)
 
-  let memory = Handlers.gen_handlers slot_list (fun i (s: (module SLOT)) ->
+  effect VRestriction: (unit -> 'a) -> 'a
+
+  (* Handles the ambient mailbox state for each slot. *)
+  (* Projecting to a uniformly-typed list of abstract SLOT modules makes it easier to generate handlers of
+     generative effects.  *)
+  let memory = Handlers.gen slot_list (fun i (s: (module SLOT)) ->
                    let module S = (val s) in
                    let mem: S.t list ref = ref [] in
                    (fun action ->
@@ -214,7 +236,8 @@ module JoinImpl(J: JOIN) = struct
                      | effect (S.SetMail l) k -> mem := l; continue k ()
                    ))
 
-  let forAll = Handlers.gen_handlers slot_list (fun i (s: (module SLOT)) ->
+  (* Default behavior: enqueue each observed event notification in the corresponding mailbox. *)
+  let forAll = Handlers.gen slot_list (fun i (s: (module SLOT)) ->
                    let module S = (val s) in
                    (fun action ->
                      try action () with
@@ -222,7 +245,8 @@ module JoinImpl(J: JOIN) = struct
                         S.(setMail (x :: (getMail ())));
                         continue k (S.push x)))
 
-  let reify = Handlers.gen_handlers slot_list (fun i (s: (module SLOT)) ->
+  (* Implements the generic cartesian product.  *)
+  let reify = Handlers.gen slot_list (fun i (s: (module SLOT)) ->
                    let module S = (val s) in
                    (fun action ->
                      try action () with
@@ -232,36 +256,118 @@ module JoinImpl(J: JOIN) = struct
                             | effect (S.GetMail) k -> continue k [x]
                           end
                         in
-                        cart mail (fun x -> Printf.printf "%s\n" (show x); [x]); (* TODO: put real impl here. *)
+                        List.iter (trigger) (cart mail (fun x -> [x])); (* TODO put real impl here*)
                         continue k ()))
+
+  (* To join means applying this stack of effect handlers to a computation generating push notifications.
+     If we had effect types, then the join would be an elimination form of all generative push effects
+     S1.Push ... Sn.Push in the slots hlist. *)
   let run action =
-    Handlers.with_h [memory;reify;forAll] action
+    Handlers.with_hs [memory;reify;forAll] action
 end
 
-(* tests *)
-let s1 = mkSlot 0
-let s2 = mkSlot "0"
-let s3 = mkSlot 0.0
+(* Generates the interleaved push iterations over n reactives *)
+let interleaved_bind: type a. a Slots.hlist -> a Reacts.hlist -> unit -> unit =
+  let rec thunk_list: type a. a Slots.hlist -> a Reacts.hlist -> (unit -> unit) list =
+    function
+    | Slots.Z -> (fun Reacts.Z -> [])
+    | Slots.(S (s,ss)) ->
+       let module S = (val s) in
+       let next = thunk_list ss in
+       (fun Reacts.(S (r,rs)) ->
+         (fun () -> Reacts.eat (S.push) r) :: (next rs))
+  in (fun slots ->
+      let mk_thunks = thunk_list slots in
+      fun rs () -> Async.interleaved (Array.of_list (mk_thunks rs))) (* TODO: generate the array right away *)
 
-module J3 =
-  (struct
-    type index = (int * (string * (float * unit)))
-    let show: index -> string = fun (i, (s, (f, ()))) -> Printf.sprintf "(%d,%s,%2.1f)" i s f
-    let slots = Slots.(cons s1 @@ cons s2 @@ cons s3 @@ nil)
-   end: (JOIN with type index = (int * (string * (float * unit)))))
 
-module Join3 = JoinImpl(J3)
 
-let test_join3 =
-  let module S1 = (val s1) in
-  let module S2 = (val s2) in
-  let module S3 = (val s3) in
-  Join3.run (fun () ->
-      S1.push 1;
-      S2.push "A";
-      S3.push 0.3;
-      S1.push 2;
-      S2.push "B";
-      S3.push 0.2;
-      S1.push 3;
-      S3.push 0.1)
+
+(* let mkJoin (type i j s)
+ *       (p_j: (i,j) WrapEvt.proof)
+ *       (p_s: (i,s) WrapEvtSlot.proof) =
+ *   (module struct
+ *      type index = i
+ *      type joined = j
+ *      type slots = s
+ *      let p_index_joined_rel = p_j
+ *      let p_index_slot_rel = p_s
+ *    end: (JOIN with type index = i and type joined = j and type slots = s)) *)
+
+
+(* module PolyJoin = struct (\* TODO should we have an uncurried variant? *\)
+ *   let z k = k (mkJoin WrapEvt.Base WrapEvtSlot.Base)
+ *
+ *   let s (type a b c) n k element_typ =
+ *     n (fun (v: (a, b, c) join) ->
+ *         let module J = (val v) in
+ *         let event_typ = evt_typ element_typ in
+ *         let slot_typ = slot_typ event_typ in
+ *         let proof_joined = WrapEvt.Step (element_typ, event_typ, J.p_index_joined_rel) in
+ *         let proof_slots = WrapEvtSlot.Step (element_typ, slot_typ, J.p_index_slot_rel) in
+ *         let j = mkJoin proof_joined proof_slots in
+ *         k j)
+ *
+ *   let mk n = n (fun x -> x)
+ *
+ *   (\* tests *\)
+ *   let zero   = mk z
+ *   let once   = mk (s z) (witness "1")
+ *   let twice  = mk (s (s z)) (witness "1") (witness 2)
+ *   let thrice = mk (s (s (s z))) (witness "1") (witness 2) (witness 3.0)
+ *
+ * (\*
+ *  let bad = once (witness 1) (witness 2);;
+ *            ^^^^ *\)
+ * end *)
+
+module Tests = struct
+  let s1 = mkSlot 0
+  let s2 = mkSlot "0"
+  let s3 = mkSlot 0.0
+  let s4 = mkSlot 'c'
+  let slots3 = Slots.(cons s1 @@ cons s2 @@ cons s3 @@ nil)
+  let slots4 = Slots.(cons s4 @@ slots3)
+  let list0 = ['x';'y';'z']
+  let list1 = [1;2]
+  let list2 = ["A";"B"]
+  let list3 = [0.3;0.2;0.1]
+  let reacts3 = Reacts.(cons list1 @@ cons list2 @@ cons list3 nil)
+  let reacts4 = Reacts.(cons list0 reacts3)
+  let interleave3 = interleaved_bind slots3 reacts3
+  let interleave4 = interleaved_bind slots4 reacts4
+
+  (* These could be generated with appropriate Fridlender & Indrika numerals. *)
+  module J3 =
+    (struct
+      type index = (int * (string * (float * unit)))
+      let slots = slots3
+     end: (JOIN with type index = (int * (string * (float * unit)))))
+
+  module J4 =
+    (struct
+      type index = char * (int * (string * (float * unit)))
+      let slots = slots4
+     end: (JOIN with type index = char * (int * (string * (float * unit)))))
+
+  module Join3 = JoinShape(J3)
+  module Join4 = JoinShape(J4)
+
+  let show3 action =
+    try action () with
+    | effect (Join3.Trigger (i, (s, (f, ())))) k ->
+       continue k (Printf.printf "(%d,%s,%2.1f)\n" i s f)
+
+  let show4 action =
+    try action () with
+    | effect (Join4.Trigger (c, (i, (s, (f, ()))))) k ->
+       continue k (Printf.printf "(%c,%d,%s,%2.1f)\n" c i s f)
+
+  let test_join3 () =
+    let open Handlers in
+    ((Async.run) |+| show3 |+| Join3.run) interleave3
+
+  let test_join4 () =
+    let open Handlers in
+    ((Async.run) |+| show4 |+| Join4.run) interleave4
+end
